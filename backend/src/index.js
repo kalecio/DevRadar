400aff2b4a072f1397d3e9ed3739d4b3e5218aad
kalecio <kalecio.santana@gmail.com>
2020-01-15 16:50:51 -0300

    terceiro dia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 require('dotenv/config');
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect(process.env.DATABASE_CONNECTION, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
