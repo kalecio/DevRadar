632dea5b0cae537a11d97fe30af42467d9a7bc60
kalecio <kalecio.santana@gmail.com>
2020-01-17 11:41:17 -0300

    quinto dia

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket');
 require('dotenv/config');
+
+
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect(process.env.DATABASE_CONNECTION, {
     useNewUrlParser: true,
@@ -14,4 +21,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'xj-y39.anonymous.mobile.exp.direct:3333'
+    baseURL: 'http://192.168.254.185:3333'
 });
 
 export default api;
